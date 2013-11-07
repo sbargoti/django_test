15d682ad3cb414ef6ce5e88cd9ba91fade5d6ca6
Suchet Bargoti <bargoti.suchet@gmail.com>
2013-11-07 13:40:56 +1100

    Finished tutorial 6

--- article/views.py
+++ article/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render_to_response
 from article.models import Article
 
 def articles(request):
-    return render_to_response('articles.html',
+    return render_to_response('article/articles.html',
             {'articles': Article.objects.all() })
 
 def article(request, article_id=1):
-    return render_to_response('article.html',
+    return render_to_response('article/article.html',
             {'article': Article.objects.get(id=article_id) })
